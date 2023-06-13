4b24957aa0cc82b0196f466b3a4d4547f6148ba1
Skamt <114232893+Skamt@users.noreply.github.com>
2023-06-13 22:31:58 +0100

    Update script.js

--- youtubeEmbed/script.js
+++ youtubeEmbed/script.js
@@ -2,6 +2,6 @@
 	if (host.includes("youtube.com") && (v = searchParams.get("v"))) {
 		window.stop();
 		document.documentElement.innerHTML = '';
-		document.location.href = `https://www.youtube.com/embed/${v}?hl=en&modestbranding=1&iv_load_policy=3&rel=0&showinfo=0&autoplay=1`;
+		document.location.href = `https://www.youtube-nocookie.com/embed/${v}?hl=en&modestbranding=1&iv_load_policy=3&rel=0&showinfo=0&autoplay=1`;
 	}
 })(new URL(window.location))
